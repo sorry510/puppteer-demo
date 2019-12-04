765fd9fef24e11e68d0540758252937a8f512437
sorry510 <eminemsf@gmail.com>
2019-12-04 22:18:36 +0800

    fix: 修复资源加载缓慢超时问题

--- douban/index.js
+++ douban/index.js
@@ -1,40 +1,55 @@
-require('dotenv').config()
 const puppeteer = require('puppeteer')
 
-const url = 'https://movie.douban.com/subject/26100958/'
+const subjectUrl = 'https://movie.douban.com/subject/26100958'
+// const subjectUrl = 'https://www.baidu.com'
 
 ;(async () => {
   try {
     const browser = await puppeteer.launch({
       // headless: false,
-      slowMo: 100,
-      defaultViewport: {
-      	width: 1920 - 60, 
-      	height: 950
-      },
+      // slowMo: 100,
+      // defaultViewport: {
+      // 	width: 1920 - 60, 
+      // 	height: 950
+      // },
       // devtools: true
     })
 
     // await page.waitFor(500) // 等待500ms
 
     const page = await browser.newPage();
-    await page.goto(url)
-    await page.waitForSelector('#content') // 等待元素加载之后，否则获取不异步加载的元素
-
+    console.log('start open douban url')
+    await page.goto(subjectUrl, {
+      // timeout: 0,
+      waitUntil: 'domcontentloaded'
+    })
+    console.log('open douban url')
+    await page.waitForSelector('#wrapper') // 等待元素加载之后，否则获取不异步加载的元素
+    console.log('start...')
     const img = await page.$eval('#mainpic img', el => el.src) // 电影海报
 
-    const strInfo = await page.$eval('#mainpic img', el => el.textContent) // 电影相关信息
+    console.log(img)
 
+    const strInfo = await page.$eval('#info', el => el.textContent) // 电影相关信息
+    console.log(strInfo)
     const arrInfo = strInfo.trim().split('\n').map(item=> {
       const [key, value] = item.split(':')
       return {[key.trim()]: value.trim()}
     })
-
-    const summary = await page.$eval('#link-report', el => el.innerText) // 摘要
+    console.log(arrInfo)
+    // const summaryDom = '#link-report'
+    // const issummary = await page.$(summaryDom)
+    // const summary = issummary ? await page.$eval(summaryDom, el => el.innerText) : '' // 摘要
     
-    const shortReview = await page.$eval('#comments-section .pl a', el => el.innerText) // 短评论数文字
+    // const shortReviewDom = '#comments-section .pl a'
+    // const isshortReview = await page.$(shortReviewDom)
+    // const shortReview = isshortReview ? await page.$eval(shortReviewDom, el => el.innerText) : '' // 短评论数量文字
+    // const [shortReviewCount = 0] = shortReview.match(/[0-9]+/) // 短评论数量
 
-    const [shortReviewCount = 0] = review.match(/[0-9]+/) // 短评论数量
+    // const reviewDom = '.reviews.mod.movie-content .pl a'
+    // const isreview = await page.$(reviewDom)
+    // const review = isreview ? await page.$eval(reviewDom, el => el.innerText) : '' // 影评数量文字
+    // const [reviewCount = 0] = review.match(/[0-9]+/) // 影评数量
 
     // const userInput = await page.$('#username')
     // await userInput.focus() // 定位到用户栏
